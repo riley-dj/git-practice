560a60ba1e673e6cd7497ea6bb30f087ecfc2909
RileyJ2005 <102147293+riley-dj@users.noreply.github.com>
2022-03-22 09:36:30 +0000

    added a text when user chooses 1

--- main.cs
+++ main.cs
@@ -5,6 +5,10 @@ class Program
   public static void Main (string[] args)
   {
     Console.WriteLine("pick a number between one and two");
-    int input = Convert.ToInt32(Console.ReadLine());  
+    int input = Convert.ToInt32(Console.ReadLine());
+    if(input == 1)
+    {
+      Console.WriteLine("you have chosen: " + input + " ;)");
+    }
   }
 }
