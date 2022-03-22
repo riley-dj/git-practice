bce4f8f0cd3765b100f64d013609d78b2e7651c3
RileyJ2005 <>
2022-03-22 09:19:41 +0000

    asked user to choose one or two

--- main.cs
+++ main.cs
@@ -1,7 +1,10 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Program
+{
+  public static void Main (string[] args)
+  {
+    Console.WriteLine("pick a number between one and two");
+    int input = Convert.ToInt32(Console.ReadLine());  
   }
 }
